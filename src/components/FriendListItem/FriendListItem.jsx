5198f3abaefcc285696f055de74081df6197e185
za3DopoGo <143600017+za3DopoGo@users.noreply.github.com>
2024-03-31 18:03:48 +0300

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,7 +1,7 @@
 import styles from "./FriendListItem.module.css";
 const FriendListItem = ({ avatar, name, isOnline }) => (
   <div className={styles.item}>
-    <img className={avatar} src={avatar} alt={name} width="48" />
+    <img className={styles.avatar} src={avatar} alt={name} width="48" />
     <p className={styles.name}>{name}</p>
     <span className={isOnline ? styles.statusOnline : styles.statusOffline}>
       {isOnline ? "Online" : "Offline"}
